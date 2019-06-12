c395e1cf3df778e6c95e706514cd2f8104934a5b
Kiryl <kbartashevich@carecloud.com>
2019-06-12 16:35:53 -0400

    Enriched stream app
    Signed-off-by: Kiryl <kbartashevich@carecloud.com>

--- src/main/java/nile/StreamApp.java
+++ src/main/java/nile/StreamApp.java
@@ -1,1 +1,24 @@
-package nile;public class StreamApp {  public static void main(String[] args){    String servers   = args[0];    String groupId   = args[1];    String inTopic   = args[2];    String goodTopic = args[3];    Consumer consumer = new Consumer(servers, groupId, inTopic);    PassthruProducer producer = new PassthruProducer(      servers, goodTopic);    consumer.run(producer);  }}
+package nile;
+
+import java.io.*;
+
+import com.maxmind.geoip2.DatabaseReader;
+
+public class StreamApp {
+
+  public static void main(String[] args) throws IOException {
+    String servers     = args[0];
+    String groupId     = args[1];
+    String inTopic     = args[2];
+    String goodTopic   = args[3];
+    String badTopic    = args[4];
+    String maxmindFile = args[5];
+
+    Consumer consumer = new Consumer(servers, groupId, inTopic);
+    DatabaseReader maxmind = new DatabaseReader.Builder(new
+      File(maxmindFile)).build();
+    FullProducer producer = new FullProducer(
+      servers, goodTopic, badTopic, maxmind);
+    consumer.run(producer);
+  }
+}
